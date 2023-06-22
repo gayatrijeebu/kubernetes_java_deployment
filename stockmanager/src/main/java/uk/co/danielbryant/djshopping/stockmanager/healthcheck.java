618bd6a8defa40d546194610bb661e4d92c20804
gayatrijeebu <97388879+gayatrijeebu@users.noreply.github.com>
2023-06-22 18:11:12 +0530

    Update and rename SimpleRestController to healthcheck.java

--- stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/healthcheck.java
+++ stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/healthcheck.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
  
 @RestController
-public class SimpleRestController {
+public class healthcheck {
     @GetMapping("/example")
     public String example() {
         return "Hello User !! " + new Date();
