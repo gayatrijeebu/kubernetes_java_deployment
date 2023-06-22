7cf6e1bbaa8036ff48db6c876a5dea527f796e59
gayatrijeebu <97388879+gayatrijeebu@users.noreply.github.com>
2023-06-22 17:05:13 +0530

    Update healthcheck.java

--- stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/healthcheck.java
+++ stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/healthcheck.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
  
 @RestController
-public class SimpleRestController {
+public class healthcheck.java {
     @GetMapping("/example")
     public String example() {
         return "Hello User !! " + new Date();
